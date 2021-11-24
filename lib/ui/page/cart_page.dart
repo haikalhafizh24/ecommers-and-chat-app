88d500adce9d13f916109d8d630a102623d9b976
haikalhafizh24 <haikal24muzakirillah@gmail.com>
2021-11-24 18:03:08 +0700

    added empty cart page and empty chat page

--- lib/ui/page/cart_page.dart
+++ lib/ui/page/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:storma/shared/theme.dart';
+import 'package:storma/ui/widgets/custom_button.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -9,13 +10,60 @@ class CartPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: cBgColor3,
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+        ),
         backgroundColor: cBgColor1,
+        centerTitle: true,
         title: Text(
           'Your Cart',
           style: whiteTextStyle.copyWith(
             fontSize: 18,
             fontWeight: medium,
           ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 70,
+              width: 80,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/emptyCart_icon.png'),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Your Card is Empty',
+              style: whiteTextStyle.copyWith(fontSize: 16),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Add item to your card!',
+              style: greyTextStyle,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomButton(
+              tittle: 'Add Product',
+              width: 153,
+              onTap: () {
+                Navigator.pushNamed(context, '/main-page');
+              },
+            ),
+          ],
         ),
       ),
     );
--- lib/ui/page/chatting_page.dart
+++ lib/ui/page/chatting_page.dart
@@ -7,9 +7,41 @@ class ChattingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: cBgColor3,
+        centerTitle: true,
+        title: Text(
+          'Support Message',
+          style: whiteTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+      ),
       backgroundColor: cBgColor1,
       body: Center(
-        child: Text('Chatting Page', style: whiteTextStyle)
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 70,
+              width: 80,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/message_image.png'),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'There is No Message',
+              style: whiteTextStyle.copyWith(fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
